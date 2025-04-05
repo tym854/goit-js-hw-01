34778a60076bea26aa0779e22e5c42c41d0f9b27
Tymur <buriachokt@gmail.com>
2025-04-05 17:06:13 +0200

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity, pricePerDroid) {
-return    console.log(`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`)
+return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
 }
 
 console.log(makeTransaction(5, 3000)); 
